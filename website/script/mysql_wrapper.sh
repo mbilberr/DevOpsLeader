0fdb963976ac4ba9724faf056f6648c11bcc32ed
118498 <u91794189@cbi-bs1.server.lan>
2018-06-08 17:51:30 -0400

    Cleanup

--- website/script/mysql_wrapper.sh
+++
@@ -1,1 +0,0 @@
-mysql -h db717746068.db.1and1.com -u dbo717746068 -p'$password_mysql' -Ddb717746068
--- website/storage/index.php
+++ website/storage/index.php
@@ -6,6 +6,8 @@
   <title>Storage Management</title>
   <link rel="stylesheet" type="text/css" href="https://www.michaelbilberry.com/style/storage.css">
   <script src="https://www.michaelbilberry.com/scripts/sorttable.js"></script>
+  <!--[if IE]><link rel="shortcut icon" href="image/icon/resume.ico"><![endif]-->
+  <link rel="icon" href="../image/icon/resume.ico">
 </head>
 
 <body style="background: #ADD8E6;">
@@ -15,6 +17,9 @@
       <input type="file" name="uploaded_file"></input><br />
       <input type="submit" value="Upload"></input>
     </form>
+    <form style="float:right">
+      <input type=button value="Refresh" onClick="window.location.reload()">
+    </form> 
   </div>
 
   <div id="container">
@@ -30,125 +35,142 @@
       </thead>
       <tbody>
       <?php
-        // Opens directory
-        $myDirectory=opendir("upload/");
-        
-        // Gets each entry
-        while($entryName=readdir($myDirectory)) {
-          $dirArray[]=$entryName;
-        }
-        
-        // Finds extensions of files
-        function findexts ($filename) {
-          $filename=strtolower($filename);
-          $exts=preg_split("[/\\.]", $filename);
-          $n=count($exts)-1;
-          $exts=$exts[$n];
-          return $exts;
-        }
-        
-        // Closes directory
-        closedir($myDirectory);
-        
-        // Counts elements in array
-        $indexCount=count($dirArray);
-        
-        // Sorts files
-        sort($dirArray);
-        
-        // Loops through the array of files
-        for($index=0; $index < $indexCount; $index++) {
-        
-          // Allows ./?hidden to show hidden files
-          if($_SERVER['QUERY_STRING']=="hidden")
-          {$hide="";
-          $ahref="./";
-          $atext="Hide";}
-          else
-          {$hide=".";
-          $ahref="./?hidden";
-          $atext="Show";}
-          if(substr("$dirArray[$index]", 0, 1) != $hide) {
-          
-          // Gets File Names
-          $name=$dirArray[$index];
-          $namehref=$dirArray[$index];
-          
-          // Gets Extensions 
-          $extn=findexts($dirArray[$index]); 
-          
-          // Gets file size 
-          $size=number_format(filesize($dirArray[$index]));
-          
-          // Gets Date Modified Data
-          $modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
-          $timekey=date("YmdHis", filemtime($dirArray[$index]));
-          
-          // Prettifies File Types, add more to suit your needs.
-          switch ($extn){
-            case "png": $extn="PNG Image"; break;
-            case "jpg": $extn="JPEG Image"; break;
-            case "svg": $extn="SVG Image"; break;
-            case "gif": $extn="GIF Image"; break;
-            case "ico": $extn="Windows Icon"; break;
-            
-            case "txt": $extn="Text File"; break;
-            case "log": $extn="Log File"; break;
-            case "htm": $extn="HTML File"; break;
-            case "php": $extn="PHP Script"; break;
-            case "js": $extn="Javascript"; break;
-            case "css": $extn="Stylesheet"; break;
-            case "pdf": $extn="PDF Document"; break;
-            
-            case "zip": $extn="ZIP Archive"; break;
-            case "bak": $extn="Backup File"; break;
-            
-            default: $extn=strtoupper($extn)." File"; break;
-          }
-          
-          // Separates directories
-          if(is_dir($dirArray[$index])) {
-            $extn="&lt;Directory&gt;"; 
-            $size="&lt;Directory&gt;"; 
-            $class="dir";
-          } else {
-            $class="file";
-          }
-          
-          // Cleans up . and .. directories 
-          if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;";}
-          if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
-          
-          // Print 'em
-          print("
+chdir(upload);
+$myDirectory = opendir(".");
+
+while ($entryName = readdir($myDirectory)) {
+  $dirArray[] = $entryName;
+}
+
+
+function findexts($filename)
+{
+  $filename = strtolower($filename);
+  $exts     = explode("[/\\.]", $filename);
+  $n        = count($exts) - 1;
+  $exts     = $exts[$n];
+  $exts     = substr($exts, -3, 3);
+  return $exts;
+}
+
+closedir($myDirectory);
+$indexCount = count($dirArray);
+sort($dirArray);
+for ($index = 0; $index < $indexCount; $index++) {
+  
+  if ($_SERVER['QUERY_STRING'] == "hidden") {
+    $hide  = "";
+    $ahref = "./";
+    $atext = "Hide";
+  } else {
+    $hide  = ".";
+    $ahref = "./?hidden";
+    $atext = "Show";
+  }
+  if (substr("$dirArray[$index]", 0, 1) != $hide) {
+    
+    $name     = $dirArray[$index];
+    $namehref = $dirArray[$index];
+    
+    $extn = findexts($dirArray[$index]);
+    
+    $size = number_format(filesize($dirArray[$index]));
+    
+    $modtime = date("M j Y g:i A", filemtime($dirArray[$index]));
+    $timekey = date("YmdHis", filemtime($dirArray[$index]));
+    
+    switch ($extn) {
+      case "png":
+        $extn = "PNG Image";
+        break;
+      case "jpg":
+        $extn = "JPEG Image";
+        break;
+      case "svg":
+        $extn = "SVG Image";
+        break;
+      case "gif":
+        $extn = "GIF Image";
+        break;
+      case "ico":
+        $extn = "Windows Icon";
+        break;
+      case "txt":
+        $extn = "Text File";
+        break;
+      case "log":
+        $extn = "Log File";
+        break;
+      case "htm":
+        $extn = "HTML File";
+        break;
+      case "php":
+        $extn = "PHP Script";
+        break;
+      case "js":
+        $extn = "Javascript";
+        break;
+      case "css":
+        $extn = "Stylesheet";
+        break;
+      case "pdf":
+        $extn = "PDF Document";
+        break;
+      case "zip":
+        $extn = "ZIP Archive";
+        break;
+      case "bak":
+        $extn = "Backup File";
+        break;
+      default:
+        $extn = strtoupper($extn) . " File";
+        break;
+    }
+    
+    if (is_dir($dirArray[$index])) {
+      $extn  = "&lt;Directory&gt;";
+      $size  = "&lt;Directory&gt;";
+      $class = "dir";
+    } else {
+      $class = "file";
+    }
+    
+    if ($name == ".") {
+      $name = ". (Current Directory)";
+      $extn = "&lt;System Dir&gt;";
+    }
+    if ($name == "..") {
+      $name = ".. (Parent Directory)";
+      $extn = "&lt;System Dir&gt;";
+    }
+    
+    print("
           <tr class='$class'>
-            <td><a href='./$namehref'>$name</a></td>
-            <td><a href='./$namehref'>$extn</a></td>
-            <td><a href='./$namehref'>$size</a></td>
-            <td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
+            <td><a href='upload/$namehref'>$name</a></td>
+            <td><a href='upload/$namehref'>$extn</a></td>
+            <td><a href='upload/$namehref'>$size</a></td>
+            <td sorttable_customkey='$timekey'><a href='upload/$namehref'>$modtime</a></td>
           </tr>");
-          }
-        }
-      ?>
-      </tbody>
+  }
+}
+?>
+    </tbody>
     </table>
-    <h2><?php print("<a href='$ahref'>$atext hidden files</a>"); ?></h2>
+    <h2><?php
+print("<a href='$ahref'>$atext hidden files</a>");
+?></h2>
   </div>
-</body>
-</html>
 
 <?PHP
-  if(!empty($_FILES['uploaded_file']))
-  {
-echo getenv("STORAGE_PATH");
-    $path = "upload/";
-    $path = $path . basename( $_FILES['uploaded_file']['name']);
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      echo "The file ".  basename( $_FILES['uploaded_file']['name']).
-      " has been uploaded";
-    } else{
-        echo "There was an error uploading the file, please try again!";
-    }
+if (!empty($_FILES['uploaded_file'])) {
+  $path = $path . basename($_FILES['uploaded_file']['name']);
+  if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+    echo "The file " . basename($_FILES['uploaded_file']['name']) . " has been uploaded";
+  } else {
+    echo "There was an error uploading the file, please try again!";
   }
+}
 ?>
 
+</body>
+</html>
